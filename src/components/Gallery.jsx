75bdb84a36bd4b43cd1a90a58f9d02f6e83f23d2
Tolpa-999 <139976356+Tolpa-999@users.noreply.github.com>
2025-01-01 08:34:25 +0200

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,17 +1,36 @@
-// src/components/Gallery/Gallery.js
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
 
-
-// code for our gallery component to slide through images
+// Code for our gallery component to slide through images
 const Gallery = ({ images }) => {
-
   return (
-    <Swiper style={{ width: '100%', height: '100%', cursor: 'pointer' }}>
+    <Swiper
+      style={{
+        width: '100%',
+        height: 'auto', // Ensure the height adjusts based on the content
+        cursor: 'pointer',
+      }}
+      spaceBetween={10} // Optional: space between images
+      breakpoints={{
+        640: {
+          slidesPerView: 1, // 1 slide on small screens
+        },
+        768: {
+          slidesPerView: 2, // 2 slides on medium screens
+        },
+        1024: {
+          slidesPerView: 3, // 3 slides on larger screens
+        },
+      }}
+    >
       {images.map((img, index) => (
         <SwiperSlide key={index}>
-          <img src={img} alt={img.alt} className="w-full h-auto" />
+          <img
+            src={img}
+            alt={`Image ${index}`}
+            className="w-full h-auto object-cover" // Ensure the image scales properly
+          />
         </SwiperSlide>
       ))}
     </Swiper>
